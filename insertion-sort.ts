4c8457971e68e52f7ff1a3261beb3a04ee59c683
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-05 10:22:11 -0500

    Remove pseudocode from insertionSortInPlace

--- insertion-sort.ts
+++ insertion-sort.ts
@@ -23,22 +23,6 @@ export function insertionSort(arr: number[]): number[] {
 // In-place Insertion Sort
 // Mutates the original array
 export function insertionSortInPlace(arr: number[]): number[] {
-  /*
-  Pseudocode:
-
-  Set a pointer dividing the array into sorted and unsorted halves
-  Repeat while the unsorted half is not empty:
-  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
-  - Grab the first value from the unsorted half
-  - For each value starting from the divider,
-  - Check if the value to the left is smaller than the unsorted value
-  - If so, you've reached the insertion point so exit the loop
-  - If not shift the value to the right by 1 and continue
-  - Insert the unsorted value at the break point
-  - Increment the dividing pointer and repeat
-  Return the mutated array
-  */
-
   let divider = 1;
   while (arr[divider] != null) {
     console.log(arr.join(","));
