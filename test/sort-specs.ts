af0df4e30777741464fb0bb29f140949f63b9f6f
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-05 08:24:06 -0500

    Initial commit

---
+++ test/sort-specs.ts
@@ -0,0 +1,53 @@
+import chai, { expect } from "chai";
+import spies from "chai-spies";
+
+chai.use(spies);
+
+import { insertionSort, insertionSortInPlace } from "../insertion-sort.js";
+
+describe("Basic Sorting Algorithms", function () {
+  let arr: number[];
+  let consoleSpy: any;
+
+  beforeEach(function () {
+    consoleSpy = chai.spy.on(console, "log");
+
+    arr = [2, 4, 6, 8, 1, 3, 5, 7, 9];
+  });
+
+  afterEach(() => {
+    chai.spy.restore(console, "log");
+  });
+
+  it("performs a out-of-place insertion sort", function () {
+    let newArr = insertionSort(arr);
+
+    expect(consoleSpy).on.nth(1).be.called.with("");
+    expect(consoleSpy).on.nth(2).be.called.with("9");
+    expect(consoleSpy).on.nth(3).be.called.with("7,9");
+    expect(consoleSpy).on.nth(4).be.called.with("5,7,9");
+    expect(consoleSpy).on.nth(5).be.called.with("3,5,7,9");
+    expect(consoleSpy).on.nth(6).be.called.with("1,3,5,7,9");
+    expect(consoleSpy).on.nth(7).be.called.with("1,3,5,7,8,9");
+    expect(consoleSpy).on.nth(8).be.called.with("1,3,5,6,7,8,9");
+    expect(consoleSpy).on.nth(9).be.called.with("1,3,4,5,6,7,8,9");
+
+    expect(newArr).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(arr).to.deep.equal([2, 4, 6, 8, 1, 3, 5, 7, 9]);
+  });
+
+  it("performs an in-place insertion sort", function () {
+    insertionSortInPlace(arr);
+
+    expect(consoleSpy).on.nth(1).be.called.with("2,4,6,8,1,3,5,7,9");
+    expect(consoleSpy).on.nth(2).be.called.with("2,4,6,8,1,3,5,7,9");
+    expect(consoleSpy).on.nth(3).be.called.with("2,4,6,8,1,3,5,7,9");
+    expect(consoleSpy).on.nth(4).be.called.with("2,4,6,8,1,3,5,7,9");
+    expect(consoleSpy).on.nth(5).be.called.with("1,2,4,6,8,3,5,7,9");
+    expect(consoleSpy).on.nth(6).be.called.with("1,2,3,4,6,8,5,7,9");
+    expect(consoleSpy).on.nth(7).be.called.with("1,2,3,4,5,6,8,7,9");
+    expect(consoleSpy).on.nth(8).be.called.with("1,2,3,4,5,6,7,8,9");
+
+    expect(arr).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
